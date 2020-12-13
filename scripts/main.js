e188a8a8ab4f709c7be2b9d120403623d76c4a8d
Zyad A. Ali <zyad.ali.me@gmail.com>
2020-12-13 18:30:56 +0200

    scripts,styles: add links to projects
    Update projects section to include links to each project seperately.

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,31 @@
 const githubURL = "https://api.github.com/users/sudo-sturbia/repos";
 const projectsList = document.getElementById('projects-list');
 
-// Fetch a list of projects from github.
 fetch(githubURL).then(
     function (response) {
         response.json().then(
             function (data) {
                 for (let i = 0, length = data.length; i < length; i++) {
-                    let dt = document.createElement('dt');
-                    let dd = document.createElement('dd');
+                    let title = document.createElement("dt");
+                    let titleText = document.createElement("p");
+                    let titleLink = document.createElement("a");
 
-                    dt.appendChild(document.createTextNode(data[i]["name"]));
-                    dd.appendChild(document.createTextNode(data[i]["description"]));
+                    let description = document.createElement("dd");
 
-                    projectsList.appendChild(dt);
-                    projectsList.appendChild(dd);
+                    titleLink.title = "Project on github.";
+                    titleLink.href = data[i]["html_url"];
+                    titleLink.target="_blank";
+                    titleLink.rel="noopener noreferrer";
+
+                    titleText.appendChild(document.createTextNode(data[i]["name"]));
+                    titleLink.appendChild(document.createTextNode("sudo-sturbia/" + data[i]["name"]));
+                    title.appendChild(titleText);
+                    title.appendChild(titleLink);
+
+                    description.appendChild(document.createTextNode(data[i]["description"]));
+
+                    projectsList.appendChild(title);
+                    projectsList.appendChild(description);
                 }
             }
         );
